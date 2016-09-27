7f73636c9ea0b532994f3d06a293c0611119e909
Sam Shissler <samshissler@gmail.com>
2016-09-27 01:07:35 -0600

    Joe did stuff

--- MTool.java
+++ MTool.java
@@ -1,7 +1,44 @@
+import java.util.*;
+
 public class MTool{
+    int[] toBeSorted;
+    int[] temp;
     public int[] mathSort(int[] unsorted){
-	//insert merge sort here
-	return unsorted;
+	toBeSorted = unsorted;
+	temp = toBeSorted.clone();
+	mergeSort(0, toBeSorted.length-1);
+	return toBeSorted;
+    }
+    public void mergeSort(int low, int high){
+	if(low<high){
+	    int mid = low+(high-low)/2;
+	    mergeSort(low, mid);
+	    mergeSort(mid+1, high);
+	    combine(low, mid, high);
+	}
+    }
+    public void combine(int low, int mid, int high){
+	for(int i=low; i<=high; i++){
+	    temp[i] = toBeSorted[i];
+	}
+	int l = low;
+	int m = mid+1;
+	int index = low;
+	while(l<=mid && m<=high){
+	    if(temp[l] < temp[m]){
+		toBeSorted[index] = temp[l];
+		l++;
+	    }else{
+		toBeSorted[index] = temp[m];
+		m++;
+	    }
+	    index++;
+	}
+	while(l<=mid){
+	    toBeSorted[index] = temp[l];
+	    l++;
+	    index++;
+	}
     }
 }
 
