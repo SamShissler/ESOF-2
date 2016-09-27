5eeb4ed5c9a73d3aef648aaf31578d0bcb5ba1db
Sam Shissler <samshissler@gmail.com>
2016-09-27 00:24:33 -0600

    Bubble Sort

--- MyMath.java
+++ MyMath.java
@@ -1,6 +1,17 @@
 public class MyMath implements EducationalMathematicsSoftware{
     public int[] mathSort(int[] unsorted){
-	//insert bubble sort here
+	boolean sorted = false;
+	while(!sorted){
+	    sorted = true;
+	    for(int i=0; i<unsorted.length-1; i++){
+		if(unsorted[i]>unsorted[i+1]){
+		    int temp = unsorted[i];
+		    unsorted[i] = unsorted[i+1];
+		    unsorted[i+1] = temp;
+		    sorted = false;
+		}
+	    }
+	}
 	return unsorted;
     }
 }
